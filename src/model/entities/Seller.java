1c53974324008f27e61301e5c349648c69f39150
ClaudioCarige <ccarige@gmail.com>
2022-10-26 12:19:31 -0300

    Created search list of seller by department in SellerDaoJdbc

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -95,7 +95,8 @@ public class Seller implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + ", department=" + department + "]";
+		return "Seller: \n id = " + id + ", \n name = " + name 
+			+ ",\n email = " + email + ",\n birthDate = " + birthDate 
+			+ ",\n baseSalary = " + baseSalary + ",\n department = " + department ;
 	}
 }
