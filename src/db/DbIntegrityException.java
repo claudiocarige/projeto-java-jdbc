d8de7a349dde7148eb16849a69531a4dc679ed66
ClaudioCarige <ccarige@gmail.com>
2022-10-25 21:46:25 -0300

    Second Commit

--- src/db/DbIntegrityException.java
+++ src/db/DbIntegrityException.java
@@ -4,7 +4,6 @@ public class DbIntegrityException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 	
-	//Força o construtor a repassar a msg para classe Pai (RuntimeException)
 	public DbIntegrityException(String msg) {
 		super(msg);
 	}
