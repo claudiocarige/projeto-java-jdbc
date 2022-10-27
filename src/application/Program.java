f9208d836c3f4cd8eefae9463c725e3455efaefe
ClaudioCarige <ccarige@gmail.com>
2022-10-27 12:12:49 -0300

    Created DepartmentDaoJdbc and the implementation of findById and findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,12 +32,18 @@ public class Program {
 		}
 		System.out.println("\n\n ==== TEST 4 : seller insert ====");
 		
-		Seller seller2 = new Seller(null, "Erick Nathan", "erick@gmail.com", new Date(), 3200.00, department);
-		sellerDao.insert(seller2);
-		System.out.println("Inserted New Id: " + seller2.getId());
+//		Seller seller2 = new Seller(null, "Erick Nathan", "erick@gmail.com", new Date(), 3200.00, department);
+//		sellerDao.insert(seller2);
+//		System.out.println("Inserted New Id: " + seller2.getId());
 		
 		System.out.println("\n\n ==== TEST 5 : seller Delete ====");
-		sellerDao.deleteById(30);
+		//sellerDao.deleteById(8);
 		System.out.println("Delete Id");
+		
+		System.out.println("\n\n ==== TEST 6 : seller Delete ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Cláudio Carigé");
+		sellerDao.update(seller);
+		System.out.println("Modified Seller");
 	}
 }
---
+++ src/application/Program2.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("==== TEST 1 : Department findById ====");
+		Department dep = departmentDao.findById(2);
+		System.out.println("Department: " + dep);
+		
+		System.out.println("\n\n ==== TEST 2 : Department findAll ====");
+		List<Department> list = departmentDao.findAll();
+		for (Department dep2 : list) {
+			System.out.println("\n" + dep2);
+		}
+	}
+
+}
